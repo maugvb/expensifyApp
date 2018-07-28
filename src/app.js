6be78f5dc6ebaf14501b46c5a4b78b046b61a06a
Mauricio <slashera4@gmail.com>
2018-07-28 16:44:16 +0200

    Dropdown for picking sortBy

--- src/app.js
+++ src/app.js
@@ -11,15 +11,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
-
-
-    setTimeout(()=>{
-        store.dispatch(setTextFilter('bill'));
-
-    }, 3000)
+store.dispatch(addExpense({ description: 'Water bill', amount: 1000}));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 2000 }));
+store.dispatch(addExpense({ description: 'Rent' , amount: 20000}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {setTextFilter} from '../actions/filters'
+import {setTextFilter, sortByAmount, sortByDate} from '../actions/filters'
 const ExpenseListFilters = (props) => (
     <div>
         <input   type='text' value={props.filters.text} onChange={(e)=>
         {props.dispatch(setTextFilter(e.target.value))}}/>
+
+        <select
+        value={props.filters.sortBy}
+        onChange={(e) => {
+            if(e.target.value=='amount'){
+                props.dispatch(sortByAmount({amount: props.amount}))
+            }else if(e.target.value=='date'){
+                props.dispatch(sortByDate({date: props.amount}))
+            }
+        }}
+        >
+            <option value='date'>Date</option>
+            <option value='amount'>Amount</option>
+        </select>
         
     </div>
 );
