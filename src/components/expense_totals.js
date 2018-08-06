f9389b512f936a9d0f5695e5c8197c38cce6afd5
Mauricio <slashera4@gmail.com>
2018-08-06 19:12:43 +0200

    Implemented price with filters

--- src/components/expense_totals.js
+++ src/components/expense_totals.js
@@ -2,6 +2,7 @@ import React from 'react';
 import expensesTotal from '../selectors/expenses_total';
 import {connect} from 'react-redux';
 import numeral from 'numeral';
+import selectExpenses from '../selectors/expenses';
 
 const Totals = (props) => (
     <div>Totals: {numeral(expensesTotal(props.expenses) / 100).format('$0,0.00')}</div>
@@ -10,7 +11,7 @@ const Totals = (props) => (
 
 const mapStateToProps = (state) => {
     return {
-    expenses: state.expenses
+    expenses: selectExpenses(state.expenses, state.filters)
     }
 }
 
