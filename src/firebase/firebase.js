8047ee596faac0d8690d66394f5719a884910bfb
Mauricio <slashera4@gmail.com>
2018-08-19 14:35:32 +0200

    Creating a new atabase for tests

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 import * as expenseActions from '../actions/expenses';
 
 const config = {
-    apiKey: "AIzaSyALX-OU91xdEBqhol_2EaUV4BPgzuUB4n8",
-    authDomain: "expensify-feb38.firebaseapp.com",
-    databaseURL: "https://expensify-feb38.firebaseio.com",
-    projectId: "expensify-feb38",
-    storageBucket: "expensify-feb38.appspot.com",
-    messagingSenderId: "595230419853"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 
