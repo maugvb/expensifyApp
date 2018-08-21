ac85a1646b4eccbcb585e13ae816b5c7822b4f26
Mauricio <slashera4@gmail.com>
2018-08-21 18:39:38 +0200

    Added log in feature

--- src/routers/app_router.js
+++ src/routers/app_router.js
@@ -6,16 +6,19 @@ import EditExpensePage from '../components/edit_expense_page';
 import HelpPage from '../components/help_page';
 import NotFoundPage from '../components/not_found_page';
 import Header from '../components/header';
+import LoginPage from '../components/login_page';
+
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Header />
       <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/" component={LoginPage} exact={true} />
         <Route path="/create" component={AddExpensePage} />
         <Route path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
+        <Route path="/dash" component={ExpenseDashboardPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
