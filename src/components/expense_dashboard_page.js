ba986732a48f45acc4d103f05ab40955457774d4
Mauricio <slashera4@gmail.com>
2018-08-31 14:33:40 +0200

    styles

--- src/components/expense_dashboard_page.js
+++ src/components/expense_dashboard_page.js
@@ -4,9 +4,9 @@ import ExpenseListFilters from './expense_list_filters';
 import Totals from './expense_totals'
 const ExpenseDashboardPage = () => (
   <div>
+    <Totals />
     <ExpenseListFilters />
     <ExpenseList />
-    <Totals />
   </div>
 );
 
--- src/components/expense_totals.js
+++ src/components/expense_totals.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import expensesTotal from '../selectors/expenses_total';
 import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses_total';
 
-const Totals = (props) => (
-    <div>Totals: {numeral(expensesTotal(props.expenses) / 100).format('$0,0.00')}</div>
-);
+const Totals = ({ expenseCount, expensesTotal }) => {
+    const expenseWord = expenseCount === 1 ? 'expense' :'expenses';
+
+    const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00')
+    return (
+
+    <div className='page-header'>
+        <div className='content-container' >
+            <h1 className='page-header__title' >Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal} </span></h1>
+            <div className='page_header__actions' >
+                <Link className='button' to='/create'>Add Expense</Link>
+            </div>
+        </div>
+    </div>
+)}
 
 
 const mapStateToProps = (state) => {
+    const visibleExpenses = selectExpenses(state.expenses, state.filters);
     return {
-    expenses: selectExpenses(state.expenses, state.filters)
+        expenseCount: visibleExpenses.length,
+        expensesTotal: selectExpensesTotal(visibleExpenses)
     }
 }
 
