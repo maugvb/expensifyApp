fb04c81105e0da1a32007c6cd06172405385bc51
Mauricio <slashera4@gmail.com>
2018-09-01 04:10:33 +0200

    a lot of features

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/loading_page';
 const store = configureStore();
 
 const jsx = (
@@ -24,25 +25,25 @@ const renderApp = () => {
     hasRendered = true;
   }
 };
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    store.dispatch(login(user.uid))
-    store.dispatch(startSetExpenses()).then(() => {
-      renderApp();
+ firebase.auth().onAuthStateChanged((user) => {
+   if (user) {
+     store.dispatch(login(user.uid))
+     store.dispatch(startSetExpenses()).then(() => {
+       renderApp();
 
-      if (history.location.pathname === '/') {
-        history.push('/dashboard');
-      }
-    });
+       if (history.location.pathname === '/') {
+         history.push('/dashboard');
+       }
+     });
 
 
-  } else {
+   } else {
 
-    store.dispatch(logout())
-    renderApp();
-    history.push('/');
-  }
-});
+     store.dispatch(logout())
+     renderApp();
+     history.push('/');
+   }
+ });
